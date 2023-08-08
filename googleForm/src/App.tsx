cb4f67f892be354b15cdd5de273420d2afa01a92
H0zzae <39540561+H0zzae@users.noreply.github.com>
2023-08-09 06:20:36 +0900

    custom theme

--- googleForm/src/App.tsx
+++ googleForm/src/App.tsx
@@ -6,6 +6,24 @@ import {UserService} from "./services/userService";
 import Counter from "./screens/Counter";
 import Todo from "./screens/Todo";
 import Form from "./pages/Froms/form";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import { deepPurple } from '@mui/material/colors';
+
+const theme = createTheme({
+  typography : {
+    allVariants : {
+      fontSize: 14,
+    }
+  },
+  palette: {
+    primary: {
+      main: deepPurple[500],
+    },
+    secondary: {
+      main: deepPurple[100],
+    },
+  }
+});
 function App() {
   const dispatch = useAppDispatch();
   const getUser = async () =>{
@@ -14,14 +32,18 @@ function App() {
     ))
   }
   return (
-  <div className={"wrap"}>
-    <Form />
-    {/*<Counter />*/}
-    {/*<hr />*/}
-    {/*<Todo />*/}
-    {/*<p />*/}
-    {/*<button onClick={getUser}>[API TEST(GET)]</button>*/}
-  </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <div className={"wrap"}>
+          <Form />
+          {/*<Counter />*/}
+          {/*<hr />*/}
+          {/*<Todo />*/}
+          {/*<p />*/}
+          {/*<button onClick={getUser}>[API TEST(GET)]</button>*/}
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 

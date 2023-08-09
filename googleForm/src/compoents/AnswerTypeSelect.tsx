57d0f779a4c27f075d28e93116c283148c3b3ec8
H0zzae <39540561+H0zzae@users.noreply.github.com>
2023-08-09 19:41:23 +0900

    icon component 수정

--- googleForm/src/compoents/AnswerTypeSelect.tsx
+++ googleForm/src/compoents/AnswerTypeSelect.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from "react"
-import {Icon, MenuItem, Select, SelectChangeEvent, Typography} from "@mui/material";
+import React, {useEffect, useState} from "react"
+import { MenuItem, Select, SelectChangeEvent, Typography} from "@mui/material";
 import ShortTextIcon from '@mui/icons-material/ShortText';
 import SubjectIcon from '@mui/icons-material/Subject';
 import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
@@ -8,12 +8,12 @@ import ArrowDropDownCircleOutlinedIcon from '@mui/icons-material/ArrowDropDownCi
 import {FlexLeftRow} from "./ComponentStyle";
 import Divider from "@mui/material/Divider";
 
-const AnswerType = [{id: 0, icon : ShortTextIcon, name : '단답형', description : 'shortAnswer'},
-    {id: 1, icon : SubjectIcon, name : '장문형', description : 'longAnswer'},
+const AnswerType = [{id: 0, name : '단답형', description : 'shortAnswer'},
+    {id: 1, name : '장문형', description : 'longAnswer'},
     {name: 'divider'},
-    {id: 2, icon : RadioButtonCheckedIcon, name : '객관식 질문', description : 'multipleChoice'},
-    {id: 3, icon : CheckBoxOutlinedIcon, name : '체크박스', description : 'checkBox'},
-    {id: 4, icon : ArrowDropDownCircleOutlinedIcon, name : '드롭다운', description : 'dropDown'},
+    {id: 2, name : '객관식 질문', description : 'multipleChoice'},
+    {id: 3, name : '체크박스', description : 'checkBox'},
+    {id: 4, name : '드롭다운', description : 'dropDown'},
     ]
 
 interface selectedType{
@@ -26,6 +26,7 @@ export const AnswerTypeSelect =(info : selectedType) => {
     const handleChange = (event: SelectChangeEvent) => {
         setType(event.target.value);
     };
+
     return(
         <Select
             labelId="demo-simple-select-helper-label"
@@ -40,7 +41,12 @@ export const AnswerTypeSelect =(info : selectedType) => {
                     :
                     <MenuItem value={i.description}>
                         <FlexLeftRow paddingTop={8} paddingBottom={8} paddingLeft={12} gap={12}>
-                            <Icon component={i.icon} />
+                            {i.description === 'shortAnswer' ? <ShortTextIcon />
+                            : i.description === 'longAnswer' ? <SubjectIcon />
+                            : i.description ==='multipleChoice' ? <RadioButtonCheckedIcon />
+                            : i.description ==='checkBox' ? <CheckBoxOutlinedIcon />
+                            : <ArrowDropDownCircleOutlinedIcon />
+                            }
                             <Typography variant='body1'>{i.name}</Typography>
                         </FlexLeftRow>
                     </MenuItem>

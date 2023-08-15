05c9d4e016cba6c0e2898e921c9806852e17dcd1
H0zzae <39540561+H0zzae@users.noreply.github.com>
2023-08-15 23:38:03 +0900

    selected 옵션 설정

--- googleForm/src/compoents/MultipleChoiceOption.tsx
+++ googleForm/src/compoents/MultipleChoiceOption.tsx
@@ -29,13 +29,20 @@ export const MultipleChoiceOption = (info : optionInfo) => {
     const {value} = useAppSelector(state => state.user);
     const dispatch = useAppDispatch();
     const [newInputText, setNewInputText] = useState<string>();
-    const [checkedValue, setCheckedValue] = useState('');
+    const [checkedValue, setCheckedValue] = useState<string>('undefined');
     const [optionList, setOptionList] = useState<any[]>([]);
     const defaultText = "옵션 "
 
     useEffect(() => {
         setOptionList(formList[info.id].options || []);
     }, [formList, dispatch]);
+    useEffect(() => {
+        setCheckedValue('undefined');
+        const initial = optionList.map((i) => {
+            return {...i, selected:false}
+        })
+        saveModOption(initial)
+    }, [value]);
     const saveModOption = (changed:any[]) => {
         const modForm = formList.map((i) => {
             if (i.id ===info.id) {
@@ -64,10 +71,16 @@ export const MultipleChoiceOption = (info : optionInfo) => {
         const targetForm = formList[info.id];
         const newID = targetForm?.options?.length; //맨 마지막에 위치하게끔..수정필요
         const modOption = targetForm?.options?.concat([{id: newID, value: '기타', selected : false}]);
-        console.log(modOption);
+        // console.log(modOption);
     },[dispatch, formList]);
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setCheckedValue((event.target as HTMLInputElement).value);
+        const modOption = optionList.map((i) => {
+            if (i.id == event.target.id) {
+                return {...i, selected : !(i.selected)}
+            }else return {...i, selected : false}
+        });
+        saveModOption(modOption);
     };
 
     const addOptionValue = useCallback((event : FocusEvent<HTMLInputElement>) => {
@@ -75,7 +88,6 @@ export const MultipleChoiceOption = (info : optionInfo) => {
             const targetForm = formList[info.id];
             const newID = targetForm?.options?.length;
             const modOption = targetForm?.options?.concat([{id: newID, value: event.target.value, selected: false}]);
-            console.log(modOption);
             saveModOption(modOption || []);
             setNewInputText('');
         }
@@ -84,26 +96,26 @@ export const MultipleChoiceOption = (info : optionInfo) => {
     const addOptionValueWithKeyBoard = useCallback((event:KeyboardEvent<HTMLInputElement>) => {
         if (event.key ==='Enter') {
             const NewID = optionList.length;
-            const modOption = optionList.concat([{id : NewID, value : event.currentTarget.value, selected : false}]);
+            const textValue = event.currentTarget.value ==='' ? defaultText + (NewID+1) : event.currentTarget.value;
+            const modOption = optionList.concat([{id : NewID, value : textValue, selected : false}]);
             saveModOption(modOption);
             setNewInputText('')
         }
     },[dispatch, formList])
     const removeOption = useCallback((id:number) => {
         const copyForm = [...optionList];
-        console.log("copyForm", copyForm);
         const removeIndx = copyForm?.findIndex(item => item.id ===id);
         if (removeIndx!==undefined) {
             copyForm?.splice(removeIndx, 1);
-            console.log(id, removeIndx, copyForm)
             saveModOption(copyForm || []);
         }
     },[dispatch, formList])
-    const controlProps = (item : string) => ({
-        checked : checkedValue ===item,
+    const controlProps = (item : any) => ({
+        id : item?.id,
+        checked : item?.selected,
         onChange : handleChange,
-        value : item,
-        inputProps : {'aria-label' : item},
+        value : item?.value,
+        inputProps : {'aria-label' : item?.value},
     })
 
     return (
@@ -117,20 +129,25 @@ export const MultipleChoiceOption = (info : optionInfo) => {
                 {optionList?.map((item) => (
                     <FlexLeftRow  justifyContent={'space-between'}>
                         <FlexLeftRow>
-                            <Radio {...controlProps(defaultText + (optionList.length + 1).toString())} disabled />
-                            <Input //defaultValue={defaultText + (optionList.length + 1).toString()}
+                            <Radio {...controlProps(item)} disabled={value==='write'} />
+                            {value==='write' ?
+                                <Input //defaultValue={defaultText + (optionList.length + 1).toString()}
                                    value={item?.value}
                                    sx={{margin: '9px 0', width:  600}}
                                    id={item?.id}
                                    onChange={(e:ChangeEvent<HTMLInputElement>) => handleTextChange(e, item.id)}
-                            />
+                                />
+                            : <Typography>{item.value}</Typography>
+                            }
                             {/*hover시 표시됨*/}
                         </FlexLeftRow>
-                        <Tooltip title="이미지 추가">
-                            <ImageOutlinedIcon sx={{fontSize: 24}} />
-                        </Tooltip>
+                        {value==='write' &&
+                            <Tooltip title="이미지 추가">
+                                <ImageOutlinedIcon sx={{fontSize: 24}} />
+                            </Tooltip>
+                        }
                         {/*해당 질문 클릭시에만 표시됨*/}
-                        {optionList.length>1 &&
+                        {optionList.length>1 && value==='write' &&
                             <Tooltip title="삭제">
                                 <IconButton arai-label="clear" onClick={() => removeOption(item.id)}>
                                     <ClearIcon />
@@ -140,22 +157,24 @@ export const MultipleChoiceOption = (info : optionInfo) => {
                     </FlexLeftRow>
                 ))}
                 {/*해당 질문 클릭시에만 표시됨*/}
-                <FlexLeftRow paddingLeft={0}>
-                    <Radio disabled />
-                    <FlexLeftRow gap={8}>
-                        <TextField placeholder={"옵션 추가"} sx={{width: 52 }}
-                                   variant="standard"
-                                   value={newInputText}
-                                   onChange={(event : ChangeEvent<HTMLInputElement>) => setNewInputText(event.target.value)}
-                                   onBlur ={addOptionValue}
-                                   InputProps={{
-                                       onKeyDown:(event:KeyboardEvent<HTMLInputElement>) => {addOptionValueWithKeyBoard(event)}
-                                   }}
-                        />
-                        <Typography variant={"body1"}> 또는 </Typography>
-                        <Button variant={"text"} size={"small"} sx={{color: "#1976d2"}} onClick={addETC}>'기타' 추가</Button>
+                {value==='write' &&
+                    <FlexLeftRow paddingLeft={0}>
+                        <Radio disabled value={'justIcon'}/>
+                        <FlexLeftRow gap={8}>
+                            <TextField placeholder={"옵션 추가"} sx={{width: 52 }}
+                                       variant="standard"
+                                       value={newInputText}
+                                       onChange={(event : ChangeEvent<HTMLInputElement>) => setNewInputText(event.target.value)}
+                                       onBlur ={addOptionValue}
+                                       InputProps={{
+                                           onKeyDown:(event:KeyboardEvent<HTMLInputElement>) => {addOptionValueWithKeyBoard(event)}
+                                       }}
+                            />
+                            <Typography variant={"body1"}> 또는 </Typography>
+                            <Button variant={"text"} size={"small"} sx={{color: "#1976d2"}} onClick={addETC}>'기타' 추가</Button>
+                        </FlexLeftRow>
                     </FlexLeftRow>
-                </FlexLeftRow>
+                }
             </RadioGroup>
         </FormControl>
     )

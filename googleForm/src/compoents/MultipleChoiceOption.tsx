ffc9e92a3fbb614a7bb77279d1f0e9610459be96
H0zzae <39540561+H0zzae@users.noreply.github.com>
2023-08-15 21:11:43 +0900

    객관식 항목 수정 및 옵션추가 버튼 구현

--- googleForm/src/compoents/MultipleChoiceOption.tsx
+++ googleForm/src/compoents/MultipleChoiceOption.tsx
@@ -1,22 +1,84 @@
-import React, {useState} from "react"
+import React, {ChangeEvent, FocusEvent, ChangeEventHandler, useCallback, useEffect, useState} from "react"
 import {FlexLeftRow} from "./ComponentStyle";
-import {Button, FormControl, FormControlLabel, IconButton, Input, Radio, RadioGroup, Typography} from "@mui/material";
+import {
+    Button,
+    FormControl,
+    FormControlLabel,
+    IconButton,
+    Input,
+    Radio,
+    RadioGroup,
+    TextField,
+    Typography
+} from "@mui/material";
 import {ShortAnswerInputBox} from "./ShortAnswerInputBox";
 import ImageOutlinedIcon from "@mui/icons-material/ImageOutlined";
 import ClearIcon from '@mui/icons-material/Clear';
 import Tooltip from "@mui/material/Tooltip";
+import {useAppDispatch, useAppSelector} from "../research/config";
+import {setForm} from "../research/slices/formSlice";
 
-export const MultipleChoiceOption = () => {
-    const [value, setValue] = useState('');
-    const [optionList, setOptionList] = useState<string[]>([]);
+interface optionInfo {
+    id : number;
+    type ?: string;
+}
+
+export const MultipleChoiceOption = (info : optionInfo) => {
+    const {formList} = useAppSelector(state => state.form);
+    const {value} = useAppSelector(state => state.user);
+    const dispatch = useAppDispatch();
+    const [newInputText, setNewInputText] = useState<string>();
+    const [checkedValue, setCheckedValue] = useState('');
+    const [optionList, setOptionList] = useState<any[]>([]);
     const defaultText = "옵션 "
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue((event.target as HTMLInputElement).value);
+    useEffect(() => {
+        setOptionList(formList[info.id].options || []);
+    }, [formList]);
+    const saveModOption = (changed:any[]) => {
+        const modForm = formList.map((i) => {
+            if (i.id ===info.id) {
+                return {...i, options : changed}
+            }else return i
+        })
+        dispatch(setForm(modForm));
+    }
+    const handleTextChange = useCallback((event : ChangeEvent<HTMLInputElement>, id:number) => {
+        const targetForm = formList[info.id];
+        const modOption = targetForm.options?.map((i) => {
+            if (i?.id === id) {
+                return {...i, value: event.target.value}
+            } else return i
+        });
+        const modForm = formList.map((i) => {
+            if (i.id === info.id) {
+                return {...i, options: modOption}
+            } else return i
+        })
+        dispatch(setForm(modForm));
+    },[dispatch, formList]);
+
+    const addETC = useCallback(() => {
+        const targetForm = formList[info.id];
+        const newID = targetForm?.options?.length; //맨 마지막에 위치하게끔..수정필요
+        const modOption = targetForm?.options?.concat([{id: newID, value: '기타', selected : false}]);
+        console.log(modOption);
+    },[dispatch, formList]);
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCheckedValue((event.target as HTMLInputElement).value);
     };
 
+    const addOptionValue = useCallback((event : FocusEvent<HTMLInputElement>) => {
+        const targetForm = formList[info.id];
+        const newID = targetForm?.options?.length;
+        const modOption = targetForm?.options?.concat([{id: newID, value: event.target.value, selected: false}]);
+        console.log(modOption);
+        saveModOption(modOption || []);
+        setNewInputText('');
+    },[dispatch, formList])
+
     const controlProps = (item : string) => ({
-        checked : value ===item,
+        checked : checkedValue ===item,
         onChange : handleChange,
         value : item,
         inputProps : {'aria-label' : item},
@@ -27,32 +89,43 @@ export const MultipleChoiceOption = () => {
             <RadioGroup
                 aria-labelledby="demo-controlled-radio-buttons-group"
                 name="controlled-radio-buttons-group"
-                value={value}
+                value={checkedValue}
                 onChange={handleChange}
             >
-                <FlexLeftRow  justifyContent={'space-between'}>
-                    <FlexLeftRow>
-                        <Radio {...controlProps(defaultText + (optionList.length + 1).toString())} disabled />
-                        <Input defaultValue={defaultText + (optionList.length + 1).toString()} sx={{margin: '9px 0', width:  600}}/>
-                        {/*hover시 표시됨*/}
+                {optionList?.map((item) => (
+                    <FlexLeftRow  justifyContent={'space-between'}>
+                        <FlexLeftRow>
+                            <Radio {...controlProps(defaultText + (optionList.length + 1).toString())} disabled />
+                            <Input //defaultValue={defaultText + (optionList.length + 1).toString()}
+                                   value={item?.value}
+                                   sx={{margin: '9px 0', width:  600}}
+                                   id={item?.id}
+                                   onChange={(e:ChangeEvent<HTMLInputElement>) => handleTextChange(e, item.id)}
+                            />
+                            {/*hover시 표시됨*/}
+                        </FlexLeftRow>
+                        <Tooltip title="이미지 추가">
+                            <ImageOutlinedIcon sx={{fontSize: 24}} />
+                        </Tooltip>
+                        {/*해당 질문 클릭시에만 표시됨*/}
+                        <Tooltip title="삭제">
+                            <IconButton arai-label="clear">
+                                <ClearIcon />
+                            </IconButton>
+                        </Tooltip>
                     </FlexLeftRow>
-                    <Tooltip title="이미지 추가">
-                        <ImageOutlinedIcon sx={{fontSize: 24}} />
-                    </Tooltip>
-                    {/*해당 질문 클릭시에만 표시됨*/}
-                    <Tooltip title="삭제">
-                        <IconButton arai-label="clear">
-                            <ClearIcon />
-                        </IconButton>
-                    </Tooltip>
-                </FlexLeftRow>
+                ))}
                 {/*해당 질문 클릭시에만 표시됨*/}
                 <FlexLeftRow paddingLeft={0}>
                     <Radio disabled />
                     <FlexLeftRow gap={8}>
-                        <Input placeholder={"옵션 추가"} sx={{width: 52 }}/>
+                        <TextField placeholder={"옵션 추가"} sx={{width: 52 }}
+                                   variant="standard"
+                                   value={newInputText}
+                                   onBlur ={addOptionValue}
+                        />
                         <Typography variant={"body1"}> 또는 </Typography>
-                        <Button variant={"text"} size={"small"} sx={{color: "#1976d2"}}>'기타' 추가</Button>
+                        <Button variant={"text"} size={"small"} sx={{color: "#1976d2"}} onClick={addETC}>'기타' 추가</Button>
                     </FlexLeftRow>
                 </FlexLeftRow>
             </RadioGroup>
